34e098e52904aa9555464f3b0cca25072decd677
Max Gonzih <gonzih@gmail.com>
2012-03-06 11:47:52 +0300

    add battery widget

--- rc.lua
+++ rc.lua
@@ -314,6 +314,24 @@ memwidget = widget({ type = "textbox" })
 -- Register widget
 vicious.register(memwidget, vicious.widgets.mem, "$1% ($2MB/$3MB)", 13)
 
+-- Battery widget
+batdivider = nil
+batwidget  = nil
+battery    = "BAT0"
+local battery_input = io.open("/sys/class/power_supply/" .. battery .. "/present", "r")
+if battery_input ~= nil then
+   batwidget = awful.widget.progressbar()
+   batwidget:set_width(8)
+   batwidget:set_height(10)
+   batwidget:set_vertical(true)
+   batwidget:set_background_color("#494B4F")
+   batwidget:set_border_color(nil)
+   batwidget:set_color("#AECF96")
+   batwidget:set_gradient_colors({ "#AECF96", "#88A175", "#FF5656" })
+   vicious.register(batwidget, vicious.widgets.bat, "$2", 61, battery)
+   batdivider = divider
+end
+
 
 dummyw = widget ({type = "textbox" })
 dummyw.text = " "
@@ -358,6 +376,8 @@ for s = 1, screen.count() do
         s == 1 and mysystray or nil,
         dummyw,
         divider,
+        batwidget,
+        batdivider,
         memwidget,
         divider,
         cpuwidget,
