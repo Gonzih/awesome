ad37d11178493e495ed6e9f652ef6ab06b55aa19
Max Gonzih <gonzih@gmail.com>
2012-03-06 11:58:57 +0300

    add working battery widget

--- rc.lua
+++ rc.lua
@@ -319,17 +319,13 @@ batdivider = nil
 batwidget  = nil
 battery    = "BAT0"
 local battery_input = io.open("/sys/class/power_supply/" .. battery .. "/present", "r")
+
 if battery_input ~= nil then
-   batwidget = awful.widget.progressbar()
-   batwidget:set_width(8)
-   batwidget:set_height(10)
-   batwidget:set_vertical(true)
-   batwidget:set_background_color("#494B4F")
-   batwidget:set_border_color(nil)
-   batwidget:set_color("#AECF96")
-   batwidget:set_gradient_colors({ "#AECF96", "#88A175", "#FF5656" })
-   vicious.register(batwidget, vicious.widgets.bat, "$2", 61, battery)
-   batdivider = divider
+   batwidget = widget({ type = "textbox" })
+   -- Register widget
+   vicious.register(batwidget, vicious.widgets.bat, "$1 $2% $3", 60, battery)
+   batdivider = widget ({type = "textbox" })
+   batdivider.text = " | "
 end
 
 
