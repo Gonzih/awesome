e37f533686c8aa76e330f7f658e2ebb503702e34
Max Gonzih <gonzih@gmail.com>
2012-03-06 11:02:44 +0300

    rename shifty tag on p, run code on r

--- rc.lua
+++ rc.lua
@@ -390,7 +390,7 @@ globalkeys = awful.util.table.join(
                   awful.tag.viewonly(t)
               end),
     awful.key({modkey}, "a", shifty.add), -- creat a new tag
-    awful.key({modkey,}, "r", shifty.rename), -- rename a tag
+    awful.key({modkey,}, "p", shifty.rename), -- rename a tag
     awful.key({modkey, "Shift"}, "a", -- nopopup new tag
     function()
         shifty.add({nopopup = true})
@@ -440,7 +440,7 @@ globalkeys = awful.util.table.join(
         function() awful.layout.inc(layouts, -1) end),
 
     -- Prompt
-    awful.key({modkey}, "F1", function()
+    awful.key({modkey}, "r", function()
         awful.prompt.run({prompt = "Run: "},
         mypromptbox[mouse.screen].widget,
         awful.util.spawn, awful.completion.shell,
